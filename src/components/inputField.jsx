85a6591634972455e2cba9cc0c9546df02c90a75
Mcorduk <om.corduk@gmail.com>
2023-12-27 16:32:45 +0300

    feat: Add InputField component for future forms

---
+++ src/components/inputField.jsx
@@ -0,0 +1,27 @@
+export default function InputField({
+  id,
+  type = "text",
+  text = "Input Name",
+  placeholder = "Type Here...",
+}) {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <>
+      <label htmlFor={name}>{text}:</label>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        value={inputValue}
+        placeholder={placeholder}
+        onChange={handleChange}
+      ></input>
+      <p>Input Value: {inputValue}</p>
+    </>
+  );
+}
