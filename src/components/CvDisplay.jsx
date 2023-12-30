c3def4d75c0508f9d6777d8ddf44098dd53df23a
Mcorduk <om.corduk@gmail.com>
2023-12-30 17:37:09 +0300

    feat: pass props to each cvdisplay section

--- src/components/CvDisplay.jsx
+++ src/components/CvDisplay.jsx
@@ -3,13 +3,13 @@ import CvHeader from "./Headers/CvHeader";
 import EduSection from "./Sections/EducationSection";
 import ExpSection from "./Sections/ExperienceSection";
 
-export default function CvDisplay() {
+export default function CvDisplay({ generalInfo, education, experience }) {
   return (
     <div>
       <div className=" w-1/2 p-4 border-4 m-5 h-screen fixed self-start">
-        <CvHeader />
-        <EduSection />
-        <ExpSection />
+        <CvHeader generalInfo={generalInfo} />
+        <EduSection education={education} />
+        <ExpSection experience={experience} />
       </div>
     </div>
   );
