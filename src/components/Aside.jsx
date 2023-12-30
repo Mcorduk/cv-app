146756474c47d8914e1d0febaf24350059c1596b
Mcorduk <om.corduk@gmail.com>
2023-12-30 18:29:48 +0300

    feat: EducationForm is wired up to listen for input changes

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -4,16 +4,29 @@ import { EducationForm } from "./Forms/EducationForm";
 import { ExperienceForm } from "./Forms/ExperienceForm";
 import { GeneralForm } from "./Forms/GeneralForm";
 
-export default function Aside({ generalInfo, updateGeneral }) {
+export default function Aside({
+  generalInfo,
+  updateGeneral,
+  education,
+  updateEducation,
+  experience,
+  updateExperience,
+}) {
   return (
     <div className="flex flex-col items-center min-w-100 max-w-screen-sm w-1/2 p-4 dark:bg-gray-700 gap-2">
+      {/* General Form is only created once. Only one form can be filled */}
       <GeneralForm generalInfo={generalInfo} updateGeneral={updateGeneral} />
-      <EducationForm />
+      {/* First education form to be filled. */}
+      <EducationForm education={education} updateEducation={updateEducation} />
       <Collapse text="Add Education">
         {" "}
         <EducationForm />
       </Collapse>
-      <ExperienceForm />
+      {/* First Experience form to be filled. */}
+      <ExperienceForm
+        experience={experience}
+        updateExperience={updateExperience}
+      />
       <Collapse text="Add Experience">
         <ExperienceForm />
       </Collapse>
--- src/components/Forms/EducationForm.jsx
+++ src/components/Forms/EducationForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import InputField from "./InputField";
 
-export function EducationForm() {
+export function EducationForm({ education, updateEducation }) {
   return (
     <div className="w-full max-w-lg p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-6 md:pt-4 dark:bg-gray-800 dark:border-gray-700">
       <h1 className="text-white pb-2">Education Section</h1>
@@ -11,12 +11,16 @@ export function EducationForm() {
             name="school"
             text="School"
             placeholder="Enter school / university"
+            value={education.school}
+            onChange={(e) => updateEducation("school", e.target.value)}
             className="flex-grow pr-4"
           />
           <InputField
             name="degree"
             text="Degree"
             placeholder="Enter Degree Title (Bsc., Msc. etc)"
+            value={education.degree}
+            onChange={(e) => updateEducation("degree", e.target.value)}
             className="flex-grow"
           />
         </div>
@@ -26,6 +30,8 @@ export function EducationForm() {
             type="date"
             text="Start Date"
             placeholder="Enter Start Date"
+            value={education.startDate}
+            onChange={(e) => updateEducation("startDate", e.target.value)}
             className="flex-grow pr-4"
           />
           <InputField
@@ -33,6 +39,8 @@ export function EducationForm() {
             type="date"
             text="End Date"
             placeholder="Enter End Date"
+            value={education.endDate}
+            onChange={(e) => updateEducation("endDate", e.target.value)}
             className="flex-grow"
           />
         </div>
@@ -40,6 +48,8 @@ export function EducationForm() {
           name="location"
           text="Location"
           placeholder="Enter Location"
+          value={education.location}
+          onChange={(e) => updateEducation("location", e.target.value)}
         />
       </form>
     </div>
