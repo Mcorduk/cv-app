ecd39f32401b8172ad1a189acd8aa34cefdc2f04
Mcorduk <om.corduk@gmail.com>
2023-12-31 13:40:40 +0300

    feat: wire experience state variables to experience display

--- src/components/Sections/ExperienceSection.jsx
+++ src/components/Sections/ExperienceSection.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { SectionHeader, SectionInfo } from "./EducationSection";
+import { SectionHeader, SectionInfo } from "./ExperienceSection";
 
-export default function ExpSection() {
+export default function ExpSection({ experience }) {
   return (
     <>
       <SectionHeader text="Experience Section" />
       <SectionInfo
-        startDate="08/2020"
-        location="Istanbul, Turkey"
-        institution="Aperture Science Laboratories"
-        title="Research Asistant"
-        description={
-          "Supported senior researchers on accessibility standards for the open web. Created and usability tested wireframes and prototypes. Produced interactive documentation for quick onboarding of new researchers."
-        }
+        startDate={experience.startDate}
+        endDate={experience.endDate}
+        location={experience.location}
+        institution={experience.school}
+        title={experience.degree}
+        description={experience.description}
       />
     </>
   );
